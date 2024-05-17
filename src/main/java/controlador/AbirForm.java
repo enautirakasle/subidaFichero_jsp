4dec13864fe4678fd49716745d5f29e83ba757e3
plaiaundi <plaiaundi@MININT-5OLIS4G>
2024-05-17 20:45:12 +0200

    web xml utzik laga dugu, abrir formen ruta erroa jarrita dabil

--- src/main/java/controlador/AbirForm.java
+++ src/main/java/controlador/AbirForm.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class AbirForm
  */
-@WebServlet("/AbirForm")
+@WebServlet("/")
 public class AbirForm extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
